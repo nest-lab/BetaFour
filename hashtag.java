c078f53ce3d21cb6bd163ae70023e251f004670a
Fabusuyi Ayodeji <fabgraphicsng@gmail.com>
2017-08-21 17:04:01 +0100

    Update hashtag.java

--- hashtag.java
+++ hashtag.java
@@ -1,15 +1,39 @@
-public class hashtag{
-  public static void main(String[] args){
-      String input = new Scanner(System.in);
-      System.out.println("Enter your comment");
-      String comment = input.nextLine();
-      int  count = 0;
-      for(String cmt: comment.split("")){
-        if(cmt.contains("#")){
-          count+=1;
-          System.out.println(cmt+", ");
-       }
-     }
-     System.out.println("The occurence of the "+count+" hashtags in the user comment are printed above");
-   }
- }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nestlab;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author TeamBeta
+ */
+public class hashtag {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        String counter = "";
+        System.out.println("Enter your comment");
+        String comment = input.nextLine();
+         int count=0;
+         for(String cmtCount: comment.split("")){
+             if(cmtCount.contains("#")){
+                 count+=1;
+             }
+         }
+        for(String cmtText: comment.split(" ")){
+            if(cmtText.contains("#")){
+                System.out.print(cmtText+", ");  
+            }
+        }
+           System.out.println("The occurence(s) of the "+count+" hashtags in the user comment are printed above");
+   
+         }
+         
+    }
+   
+    
+
